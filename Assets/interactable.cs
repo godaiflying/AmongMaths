1c60e59078681f4d1104a432e222e1e3d3f98b93
Skezazzle <83253371+Skezazzle@users.noreply.github.com>
2021-05-28 09:49:12 +1000

    fdsahg

--- Assets/interactable.cs
+++ Assets/interactable.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class interactable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Button Submit;
+    public Button Numpad1;
+    public Button Numpad2;
+    public Button Numpad3;
+    public Button Numpad4;
+    public Button Numpad5;
+    public Button Numpad6;
+    public Button Numpad7;
+    public Button Numpad8;
+    public Button Numpad9;
+    public Button Numpad0;
+    public Text SubmittedAnswer;
 
-    // Update is called once per frame
-    void Update()
+    void start()
     {
         
     }
