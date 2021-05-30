68ac58f114a84a4d8f8cd568578ee70e86ee65ee
Skezazzle <83253371+Skezazzle@users.noreply.github.com>
2021-05-31 09:49:46 +1000

    fsakyv

---
+++ Assets/CalcTask Components/Scripts/Keypad.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keypad : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("Calculator just woke up.");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
