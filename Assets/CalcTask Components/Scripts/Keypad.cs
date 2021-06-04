9e14200205d8ac221671461d479f0870db83af1d
Skezazzle <83253371+Skezazzle@users.noreply.github.com>
2021-06-04 14:59:42 +1000

    csas

--- Assets/CalcTask Components/Scripts/Keypad.cs
+++ Assets/CalcTask Components/Scripts/Keypad.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI
+using UnityEngine.UI;
 
 public class Keypad : MonoBehaviour
 {
+    public int inputCurrent;
+    public Button Submit;
     public Button Key1;
     public Button Key2;
     public Button Key3;
@@ -15,10 +17,15 @@ public class Keypad : MonoBehaviour
     public Button Key8;
     public Button Key9;
     public Button Key0;
-    public Text GivenAnswer;
+    public Text givenAnswer;
 
     public void Calculate()
     {
+        Key1.onClick.AddListener(Add1);
+    }
 
+    void Add1()
+    {
+        inputCurrent = inputCurrent * 10 + 1;
     }
 }
